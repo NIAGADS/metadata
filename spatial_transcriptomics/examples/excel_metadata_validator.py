d564382bfc9cb6f633a32dc0ae9922ffb540cfc8
fossilfriend <egreenfest@gmail.com>
2023-12-13 16:02:38 -0500

    fleshed out todos

--- spatial_transcriptomics/examples/excel_metadata_validator.py
+++ spatial_transcriptomics/examples/excel_metadata_validator.py
@@ -4,9 +4,8 @@ import argparse
 from os import path, getcwd
 
 from niagads.utils.logging import ExitOnExceptionHandler
-from niagads.validators import MetadataValidator, FileManifestValidator
+from niagads.validators import MetadataValidator, FileManifestValidator, BiosourcePropertiesValidator
 from niagads.utils.dict import print_dict
-from niagads.utils.string import xstr
 
 logger = logging.getLogger(__name__)
 
@@ -26,9 +25,17 @@ def run():
             errors[worksheet] = "PASSED" if isinstance(validationResult, bool) else validationResult
 
         # TODO Biosource properties validation
-        
-        fmSchema = path.join(args.schemaDir, 'file_manifest.json')
-        fmValidator = FileManifestValidator(args.excelFile, fmSchema, args.debug)
+        # schema = path.join(args.schemaDir, 'biosource_properties.json')
+        # bsValidator = BiosourcePropertiesValidator(args.excelFile, schema, args.debug)
+        # bsValidator.load('biosource_properties')
+        # if args.debug:
+        #     logger.debug(print_dict(bsValidator.get_metadata(), pretty=False))
+        # validationResult = bsValidator.run(failOnError=args.failAtFirst)
+        # errors['biosource_properties'] = "PASSED" if isinstance(validationResult, bool) else validationResult
+        # validSampleIds = bmValidator.get_sample_ids()
+         
+        schema = path.join(args.schemaDir, 'file_manifest.json')
+        fmValidator = FileManifestValidator(args.excelFile, schema, args.debug)
         fmValidator.load('file_manifest')
         if args.debug:
             logger.debug(print_dict(fmValidator.get_metadata(), pretty=False))
@@ -36,6 +43,7 @@ def run():
         errors['file_manifest'] = "PASSED" if isinstance(validationResult, bool) else validationResult
 
         # TODO file manifest sample id validation
+        # fmValidator.validate_samples('sample_id', validSamples)
         
         logger.info("DONE")
         logger.info(print_dict(errors, pretty=True))
@@ -46,9 +54,10 @@ if __name__ == "__main__":
     argParser = argparse.ArgumentParser(description="EXCEL to JSON w/Validation test", allow_abbrev=False)
     argParser.add_argument('--excelFile', help="EXCEL file name (full path)", required=True)
     argParser.add_argument('--schemaDir', help="full path to directory containing schema files", required=True)
+    argParser.add_argument('--logFile', help="log file name, saves to current working directory", 
+        default="EXCEL_validation_test.log")
     argParser.add_argument('--failAtFirst', action='store_true')
     argParser.add_argument('--debug', action='store_true')
-    argParser.add_argument('--logFile', help="log file name, full path", default="EXCEL_validation_test.log")
     
     args = argParser.parse_args()
 
